00606354dcbce38326d97d5d5d71e9109ebfc25a
Roggenrolo <rmcordero.99@gmail.com>
2022-04-07 21:25:27 -0600

    Add files via upload

---
+++ client/index.js
@@ -0,0 +1,11 @@
+const { MongoClient } = require("mongodb");
+
+const connectDatabase = () => {
+    const client = new MongoClient("mongodb://localhost:27017,localhost:27018");
+    client.connect();
+    // client.connect().then(() => {
+    //     console.log(client.db("turismo").collection("atracciones").find());
+    // });
+}
+
+connectDatabase();
