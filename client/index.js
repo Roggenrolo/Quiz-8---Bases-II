922c95dfdd1d3765d36324e24a3befd7369ae964
Roggenrolo <rmcordero.99@gmail.com>
2022-04-07 21:29:59 -0600

    Update index.js

--- client/index.js
+++ client/index.js
@@ -2,10 +2,9 @@ const { MongoClient } = require("mongodb");
 
 const connectDatabase = () => {
     const client = new MongoClient("mongodb://localhost:27017,localhost:27018");
-    client.connect();
-    // client.connect().then(() => {
-    //     console.log(client.db("turismo").collection("atracciones").find());
-    // });
+    client.connect().then(() => {
+    console.log(client.db("turismo").collection("atracciones").find());
+    });
 }
 
 connectDatabase();
